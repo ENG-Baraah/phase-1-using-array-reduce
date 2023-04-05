9ded6935bfc8262c4b63c9280f7b68269010d702
DESKTOP-SAWA\ASUS <eng.baraahmassri@gmail.com>
2023-04-06 00:30:00 +0300

    Completed Using the Array Reduce Method Assignment

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+let totalBatteries;
+function reducer(arr, reduce, init) {
+  totalBatteries = init;
+  for (const ele of arr) {
+    totalBatteries = reduce(totalBatteries, ele);
+  }
+  return totalBatteries;
+}
+function batteries(totalAmount, ele) {
+  return totalAmount + ele;
+}
 
-// Code your solution here
+reducer(batteryBatches, batteries, 0);
